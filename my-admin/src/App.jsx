9fee93a5998b3d86a9693861967c3f8db4e765e6
jade-benting <27137120@sun.ac.za>
2025-11-08 21:22:08 +0000

    Commit book api function

--- my-admin/src/App.jsx
+++ my-admin/src/App.jsx
@@ -1,7 +1,8 @@
 
 import {createTrailbaseProvider} from './ra-trailbase.js';
 import {ListGuesser, Resource, Admin, EditGuesser, ShowGuesser} from 'react-admin';
-import {AuthorList, AuthorEdit, AuthorCreate} from './Author.jsx';
+import {AuthorList, AuthorEdit, AuthorCreate, AuthorShow} from './Author.jsx';
+import { BookEdit, BookList, BookCreate, BookShow } from './Book.jsx';
 const TRAILBASE_URL = 'https://sturdy-eureka-5gpq77vvg55vfx99-4000.app.github.dev/';
 const {dataProvider} = await createTrailbaseProvider(TRAILBASE_URL);
 
@@ -9,8 +10,8 @@ const {dataProvider} = await createTrailbaseProvider(TRAILBASE_URL);
 const App = () => 
 (
 <Admin dataProvider = {dataProvider} > 
-  <Resource name = 'Author' list={AuthorList} edit={AuthorEdit} create = {AuthorCreate} show={ShowGuesser}/>  
-  
+  <Resource name = 'Author' list={AuthorList} edit={AuthorEdit} create = {AuthorCreate} show={AuthorShow}/>  
+  <Resource name = 'Book' list={BookList} edit={BookEdit} create = {BookCreate} show={BookShow}/>
 </Admin>
 );
 export default App;
