31599312c936ae795ba8461857f9615a381e73aa
jade-benting <27137120@sun.ac.za>
2025-11-09 19:54:53 +0000

    Commit generalusers api function

---
+++ my-admin/src/GeneralUser.jsx
@@ -0,0 +1,47 @@
+import { DataTable, List } from 'react-admin';
+import { Edit, NumberInput, SimpleForm, TextInput, Create } from 'react-admin';
+import { NumberField, Show, SimpleShowLayout, TextField } from 'react-admin';
+
+export const GeneraluserShow = () => (
+    <Show>
+        <SimpleShowLayout>
+            <TextField source="id" />
+            <NumberField source="profileID" />
+            <NumberField source="numberOfBooksRead" />
+            <TextField source="favouriteAuthor" />
+        </SimpleShowLayout>
+    </Show>
+);
+
+export const GeneraluserCreate = () => (
+    <Credit>
+        <SimpleForm>
+            <TextInput source="id" />
+            <NumberInput source="profileID" />
+            <NumberInput source="numberOfBooksRead" />
+            <TextInput source="favouriteAuthor" />
+        </SimpleForm>
+    </Credit>
+);
+
+export const GeneraluserEdit = () => (
+    <Edit>
+        <SimpleForm>
+            <TextInput source="id" />
+            <NumberInput source="profileID" />
+            <NumberInput source="numberOfBooksRead" />
+            <TextInput source="favouriteAuthor" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const GeneraluserList = () => (
+    <List>
+        <DataTable>
+            <DataTable.Col source="id" />
+            <DataTable.NumberCol source="profileID" />
+            <DataTable.NumberCol source="numberOfBooksRead" />
+            <DataTable.Col source="favouriteAuthor" />
+        </DataTable>
+    </List>
+);
